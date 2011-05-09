e42183bdbe484a0ef5e9cf957c369cfea5144f44
John Thomas Marino <writejm@gmail.com>
2011-05-09 16:14:17 -0400

    Extract send method into Railtie.insert

--- lib/bounce/railtie.rb
+++ lib/bounce/railtie.rb
@@ -1,5 +1,15 @@
-class Railtie < Rails::Railtie
-  config.before_initialize do
-    ActiveRecord::Base.send :include, Bounce::ActiveRecordExtension
+module Bounce
+  class Railtie < Rails::Railtie
+    initializer 'bounce.initialize' do
+      ActiveSupport.on_load(:active_record) do
+        Bounce::Railtie.insert
+      end
+    end
+  end
+
+  class Railtie
+    def self.insert
+      ActiveRecord::Base.send :include, Bounce::ActiveRecordExtension
+    end
   end
 end
