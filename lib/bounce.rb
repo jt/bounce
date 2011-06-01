bdca959c0506b38091fc55d4fac8b5e1e96dceb8
John Thomas Marino <writejm@gmail.com>
2011-06-01 11:16:25 -0400

    Consolidate into single location

--- lib/bounce.rb
+++ lib/bounce.rb
@@ -1,3 +1,25 @@
 require 'rails'
-require 'bounce/railtie'
-require 'bounce/extension'
+
+module Bounce
+  module ActiveRecordExtension
+    extend ActiveSupport::Concern
+
+    def bounce(params=nil)
+      tap { |o| params.present? ? o.update_attributes(params) : o.save }
+    end
+  end
+
+  class Railtie < Rails::Railtie
+    initializer 'bounce.initialize' do
+      ActiveSupport.on_load(:active_record) do
+        Bounce::Railtie.insert
+      end
+    end
+  end
+
+  class Railtie
+    def self.insert
+      ActiveRecord::Base.send :include, Bounce::ActiveRecordExtension
+    end
+  end
+end
--- lib/bounce/extension.rb
+++
@@ -1,9 +0,0 @@
-module Bounce
-  module ActiveRecordExtension
-    extend ActiveSupport::Concern
-
-    def bounce(params=nil)
-      tap { |o| params.present? ? o.update_attributes(params) : o.save }
-    end
-  end
-end
